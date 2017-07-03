64f7803f747fe9e25fca02fd13c479231c5b381d
Hammad <jilanih1@gmail.com>
2017-07-03 02:59:06 -0500

    modified:   ssh.py added error handling for incorrect hostname / credentials.

--- ssh.py
+++ ssh.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
-import paramiko, getpass
+import paramiko, getpass, subprocess
 
 hostname = raw_input('Please select host: ')
 username = raw_input('Please enter username: ')
 password = getpass.getpass('Please enter password: ')
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname, port=22, username=username, password=password)
+pinghost = subprocess.Popen(['ping', '-c', '1', hostname],stdout=subprocess.PIPE)
+stdout, stderr = pinghost.communicate()
+if pinghost.returncode == 0:
+	print 'Host is pingable, continuing.'
+	try:
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect(hostname, port=22, username=username, password=password)
 
-stdin,stdout,stderr = ssh.exec_command('uptime')
-type(stdin)
-upout = stdout.read()
-print upout
+		stdin,stdout,stderr = ssh.exec_command('uptime')
+		type(stdin)
+		upout = stdout.read()
+		print upout
+	except paramiko.AuthenticationException:
+		print 'Please check username and password.'
+else:
+	print 'Host is not pingable, please check network connectivity.'
