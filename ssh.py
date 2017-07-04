dc7d79e8c43c192f3939bbb18d14ef0e348a30f3
Hammad <jilanih1@gmail.com>
2017-07-04 00:50:50 -0500

    modified:   ssh.py

--- ssh.py
+++ ssh.py
@@ -2,23 +2,24 @@
 
 import paramiko, getpass, subprocess
 
-hostname = raw_input('Please select host: ')
+hostname = raw_input('Please enter hostname: ')
 username = raw_input('Please enter username: ')
-password = getpass.getpass('Please enter password: ')
+password = getpass.getpass(username + '@' + hostname + ' password: ')
 
 pinghost = subprocess.Popen(['ping', '-c', '1', hostname],stdout=subprocess.PIPE)
 stdout, stderr = pinghost.communicate()
 if pinghost.returncode == 0:
-	print 'Host is pingable, continuing.'
+	print 'Host is pingable, continuing...'
 	try:
 		ssh = paramiko.SSHClient()
 		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		ssh.connect(hostname, port=22, username=username, password=password)
 
+		ssh.connect(hostname, port=22, username=username, password=password)
 		stdin,stdout,stderr = ssh.exec_command('uptime')
 		type(stdin)
-		upout = stdout.read()
-		print upout
+		print hostname + ' uptime:'
+		print stdout.read()
+
 	except paramiko.AuthenticationException:
 		print 'Please check username and password.'
 else:
