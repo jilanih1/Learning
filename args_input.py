0b05f03541efada84a51246acc265e58242a569c
Hammad <jilanih1@gmail.com>
2017-07-05 00:36:56 -0500

    new file:   args_input.py how to input a string if an option is not selected while initially.

---
+++ args_input.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#Example: how to input a string if an option is not selected while initially.
+
+import argparse
+
+parser = argparse.ArgumentParser(usage='args_input.py -s <string>')
+parser.add_argument('-s', '--string', help='type a string to be printed.')
+args = parser.parse_args()
+
+if not args.string:
+	var = raw_input('no string entered, please enter string to be printed: ')
+else:
+	var = args.string
+
+print var
