cac02135171ae705729753e18a4c7ba4c4c7b2a1
Hammad <jilanih1@gmail.com>
2017-07-21 00:26:38 -0500

    new file:   netmiko_test.py Script trying out Netmiko module.

---
+++ netmiko_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#Script testing out Netmiko Library
+#Netmiko Link: https://pynet.twb-tech.com/blog/automation/netmiko.html
+
+#Tested in Arista vEOS (Pulls running-config from device)
+
+from __future__ import print_function
+from netmiko import ConnectHandler
+import sys, getpass
+if sys.version_info[:2] <= (2, 7):
+	input = raw_input
+
+host = input('Please enter IP address: ')
+user = input('Please enter username: ')
+pswd = getpass.getpass('Please enter password: ')
+
+#Dictionary with device information:
+device = {
+	'device_type': 'arista_eos',
+	'ip': host,
+	'username': user,
+	'password': pswd,
+	'port': 22,
+}
+
+#ConnectHandler with and without a dictionary:
+net_connect = ConnectHandler(**device)
+#net_connect = ConnectHandler(device_type='arista_eos', ip=host, username=user, password=pswd, port=22)
+
+#Enters enable mode:
+net_connect.enable()
+#Output from 'show command':
+output = net_connect.send_command_expect('show running-config')
+
+print('\033[91m' + 'running config from ' + host + ':')
+print('*' * 50 + '\033[0m')
+print(output)
+print('\033[91m' + '*' * 50 + '\033[0m')
+
+#Terminates SSH session:
+net_connect.disconnect()
