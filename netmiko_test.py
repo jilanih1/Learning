068cb10fe859946a1317ec7f9ef2d0704e10a23d
Hammad <jilanih1@gmail.com>
2017-07-22 00:19:35 -0500

    if __name__ == '__main__':

--- netmiko_test.py
+++ netmiko_test.py
@@ -10,36 +10,38 @@ import sys, getpass
 if sys.version_info[:2] <= (2, 7):
 	input = raw_input
 
-host = input('Please enter IP address: ')
-user = input('Please enter username: ')
-pswd = getpass.getpass('Please enter password: ')
+if __name__ == '__main__':
 
-#Dictionary with device information:
-device = {
-	'device_type': 'arista_eos',
-	'ip': host,
-	'username': user,
-	'password': pswd,
-	'port': 22,
-}
+	host = input('Please enter IP address: ')
+	user = input('Please enter username: ')
+	pswd = getpass.getpass('Please enter password: ')
 
-#ConnectHandler with and without a dictionary:
-net_connect = ConnectHandler(**device)
-#net_connect = ConnectHandler(device_type='arista_eos', ip=host, username=user, password=pswd, port=22)
+	#Dictionary with device information:
+	device = {
+		'device_type': 'arista_eos',
+		'ip': host,
+		'username': user,
+		'password': pswd,
+		'port': 22,
+	}
 
-#Enters enable mode:
-net_connect.enable()
-#Output from 'show command':
-output1 = net_connect.send_command_expect('show running-config')
-output2 = net_connect.send_command_expect('show running-config diffs')[:-1] 
+	#ConnectHandler with and without a dictionary:
+	net_connect = ConnectHandler(**device)
+	#net_connect = ConnectHandler(device_type='arista_eos', ip=host, username=user, password=pswd, port=22)
 
-print('\033[91m' + '*' * 50)
-print('Running-config from ' + host + ':' + '\n' + '*' * 50 + '\033[0m')
-print(output1)
-print('\033[91m' + '*' * 50 + '\033[0m')
-print('\033[91m' + 'Differences from startup-config:' + '\n' + '*' * 50 + '\033[0m')
-print(output2)
-print('\033[91m' + '*' * 50 + '\033[0m')
+	#Enters enable mode:
+	net_connect.enable()
+	#Output from show commands:
+	output1 = net_connect.send_command_expect('show running-config')
+	output2 = net_connect.send_command_expect('show running-config diffs')[:-1] 
 
-#Terminates SSH session:
-net_connect.disconnect()
+	print('\033[91m' + '*' * 50 + '\n' + 'Running-config from ' + host + ':' + '\n' + '*' * 50 + '\033[0m')
+	print(output1)
+	print('\033[91m' + '*' * 50 + '\n' + 'Differences from startup-config:' + '\n' + '*' * 50 + '\033[0m')
+	print(output2)
+	print('\033[91m' + '*' * 50 + '\033[0m')
+
+	#Terminates SSH session:
+	net_connect.disconnect()
+
+sys.exit(1)
